d8055133c6ab055e5a82028a45e78ca277ac42b6
Luisa Napier <luisanapier@Luisas-MacBook-Pro.local>
2023-08-19 15:20:46 -0700

    Create account and login completed, closet in progress

--- crud.py
+++ crud.py
@@ -1,4 +1,4 @@
-from model import db, connect_to_db, User
+from model import db, connect_to_db, User, Articles, Outfit, OutfitArticles, UserFavorites
  
 def create_user(username, name, email, password):
     """Register and return new users."""
@@ -10,9 +10,47 @@ def create_user(username, name, email, password):
     db.session.commit()
     return new_user
 
-
- 
+def add_articles(type, resource_url, color_primary, color_sec,
+                   pattern, occasion, season, user_id):
     
+    article = Articles(type=type, resource_url=resource_url,
+                       color_primary=color_primary, color_sec=color_sec,
+                       pattern=pattern, occasion=occasion,
+                       season=season, user_id=user_id)
+    
+    db.session.add(article)
+    db.session.commit()
+    return article
+
+def create_outfit(title, user_id):
+
+    outfit = Outfit(title=title, user_id=user_id)
+
+    db.session.add(outfit)
+    db.session.commit()
+    return outfit
+ 
+def outfit_articles(article_id, outfit_id):
+
+    out_articles = OutfitArticles(article_id=article_id, outfit_id=outfit_id)
+    
+    db.session.add(out_articles)
+    db.session.commit()
+    return out_articles
+
+def user_favorite(outfit_id, user_id):
+
+    user_fav = UserFavorites(outfit_id=outfit_id, user_id=user_id)
+
+    db.session.add(user_fav)
+    db.session.commit()
+    return user_fav
+
+def check_user(username):
+
+    is_user = User.query.filter(User.username == username).first()
+
+    return is_user
 
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -13,7 +13,18 @@ def homepage():
 @app.route('/login')
 def login():
 
-    return render_template('homepage.html')
+    username = request.args.get("username")
+    password = request.args.get("password")
+
+    user = crud.check_user(username) 
+    if user:
+        if password == user.password:
+            return render_template('closet.html', username=username)
+        else:
+            return redirect('/login')
+    else:
+        print("Username or password doesn't match. Try again")
+    
 
 @app.route('/register', methods=["POST"])
 def register():
@@ -23,19 +34,32 @@ def register():
     email = request.form.get("email")
     password = request.form.get("password")
 
-    crud.create_user(email, username, password, name)
-    return render_template('homepage.html', username=username, name=name,
-                           email=email, password=password)
+    username_exists = crud.check_user(username)
+    if username == username_exists.username:
+        print("Username already exists, please try a different one.")
+        return redirect('/register')
+    else:
+        crud.create_user(email=email, username=username, password=password, name=name)
+        return render_template('closet.html', username=username)
 
-
+    
 @app.route('/closet')
 def show_closet():
 
+
+
+    crud.add_articles(type=type, resource_url=resource_url,
+                       color_primary=color_primary, color_sec=color_sec,
+                       pattern=pattern, occasion=occasion,
+                       season=season, user_id=user_id)
+    crud.create_outfit(title=title, user_id=user_id)
+    crud.outfit_articles(article_id=article_id, outfit_id=outfit_id)
     return render_template('closet.html')
 
 @app.route('/favorites')
 def show_favorites():
-
+    
+    crud.user_favorite(outfit_id=outfit_id, user_id=user_id)
     return render_template('/favorites.html')
 
 if __name__ == '__main__':
